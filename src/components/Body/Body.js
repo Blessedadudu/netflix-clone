436fec9b806b02e2c04c5d86830864b80a71377f
Blessedadudu <adududuke@gmail.com>
2020-05-22 16:57:11 +0100

    secction 2 update

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -17,6 +17,39 @@ const Body = () => {
                         <video class="our-story-card-video" autoPlay="true" playsinline muted loop ><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4"/></video>
                     </div>
             </section>
+            <section className='section1'>
+                    <div className='imageRel'>
+                        {/* <h1>omage</h1> */}
+                        <img alt="" className="card" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="our-story-card-img"/>
+                       <div className='strangerCard'>
+                           <div>
+                           <img alt="" className="card" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/boxshot.png" data-uia="our-story-card-img"/>
+                           </div>
+                            <div>
+                                <h4>Stranger Things</h4>
+                                <p>Downloading...</p>
+                            </div>
+                            <div>
+                            <img alt="" className="strangeInnerCard" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/download-icon.gif" data-uia="our-story-card-img"/>
+                            </div>
+                       </div>
+                    </div>
+                    <div className='enjoy'>
+                        <h1>Download your shows to watch offline.</h1>
+                        <p>Save your favorites easily and always have something to watch.</p>
+                    </div>
+            </section>
+            <section className='section1'>
+                    <div className='enjoy'>
+                        <h1>Watch everywhere.</h1>
+                        <p>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.</p>
+                    </div>
+                    <div className='imageRel'>
+                        {/* <h1>omage</h1> */}
+                        <img alt="" className="card" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" data-uia="our-story-card-img"/>
+                        <video class="our-story-card-video" autoPlay="true" playsinline muted loop ><source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4"/></video>
+                    </div>
+            </section>
         </div>
     )
 }
