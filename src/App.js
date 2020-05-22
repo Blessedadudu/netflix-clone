1414817ae34c9ff756acc9b7e4c9ee69f497dda2
Blessedadudu <adududuke@gmail.com>
2020-05-22 18:00:36 +0100

    accordion component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Body from './components/Body/Body'
+import Body from './components/Body/Body';
+import Accordion from './components/Accordion/Accordion';
 
 function App() {
   return (
     <div>
       <Header/>
       <Body/>
+      <Accordion/>
     </div>
   );
 }
---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Accordion.scss'
+
+const Accordion = () => {
+    return (
+        <div>
+            <h1>Accordion</h1>
+        </div>
+    )
+}
+
+export default Accordion
