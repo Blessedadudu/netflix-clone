7663a2729fab49d70ebf15a886a1cf0f9d93dd28
Blessedadudu <adududuke@gmail.com>
2020-05-22 14:18:32 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to netflix clone</h1>
+      {/* <p>Welcome to netflix clone</p> */}
+      <Header/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import netflixheader from '../../assets/netflixheader.jpg';
+import './Header.scss';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Welcome to the header</h1>
+        <div className='header'> 
+            {/* <h1>Welcome to the header</h1> */}
+            <picture>
+                {/* <source media="(max-width: 500px)" srcSet={carousel1B}/>
+                <source media="(max-width: 1080px)" srcSet={carousel1A}/> */}
+                <img src={netflixheader} alt='caroimage'/>
+            </picture>
+            <div className='imageOverlay'></div>
+            <div>
+                <h1>Netflix</h1>
+                <button>Sign In</button>
+            </div>
         </div>
     )
 }
