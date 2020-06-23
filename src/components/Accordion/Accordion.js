71f49017c4eb44285bc76f62e04a4bf48e6a07fe
Blessedadudu <adududuke@gmail.com>
2020-06-23 22:05:44 +0100

    update

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -10,7 +10,7 @@ const Accordion = () => {
           question: 'What is Netflix',
           answer: 'Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.',
           answer2: 'You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There\'s always something new to discover and new TV shows and movies are added every week!',
-          open: true
+          open: false
         },
         {
           question: 'How much does Netflix cost?',
@@ -62,12 +62,10 @@ const Accordion = () => {
                             <div className='accordionHeading'>
                                 <h1>{items.question}</h1>
                                 <div  className='toggle'>
-                                    <input type="checkbox" class="checkbox" id="navi-toggle"/> 
-
-                                    <label for="navi-toggle" className="check" onClick={() => toggleAccordion(index)}>
-                                        <span class="icon">&nbsp;</span>
+                                    <input type="checkbox" className="checkbox" id="navi-toggle"/>
+                                    <label for="navi-toggle" className="check" onClick={() => toggleAccordion(index)} >
+                                        <span className="icon">&nbsp;</span>
                                     </label>
-                                    <div class="navigation__background">&nbsp;</div>
                                 </div>
                             </div>
                             <h2 className={ items.open ? 'show' : 'showText' }>{items.answer}<br/><br/>{items.answer2}</h2>
