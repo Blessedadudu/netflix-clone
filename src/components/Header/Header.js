d06433f41d345a8f92175468c55868c94801c3aa
Blessedadudu <adududuke@gmail.com>
2020-06-27 13:59:25 +0100

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import netflixheader from '../../assets/netflixheader.jpg';
 import './Header.scss';
-
+ 
 const Header = () => {
     return ( 
         <div className='header'> 
