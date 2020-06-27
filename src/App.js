aa441863e15893588920a9a0eaa7cbdf17fd7271
Blessedadudu <adududuke@gmail.com>
2020-06-27 11:20:43 +0100

    footer setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Accordion from './components/Accordion/Accordion';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Header/>
       <Body/>
       <Accordion/>
-
+      <Footer/>
     </div>
   );
 }
