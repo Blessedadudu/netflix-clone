7b835e276e57ce1a356c3606b4db9ed0580d8870
Blessedadudu <adududuke@gmail.com>
2020-06-27 11:38:30 +0100

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,28 @@ import './Footer.scss'
 export default function Footer() {
     return (
         <footer className='footer'>
-            <h1>hello</h1>
+            <h4>Questions? Contact us.</h4>
+        
+            <div>
+                <ul>
+                    <li>FAQ</li>
+                    <li>Help Center</li>
+                    <li>Account</li>
+                    <li>Media Center</li>
+                    <li>Investor Relations</li>
+                    <li>Jobs</li>
+                    <li>Ways to Watch</li>
+                    <li>Terms of Use</li>
+                    <li>Privacy</li>
+                    <li>Cookie Preferences</li>
+                    <li>Corporate Information</li>
+                    <li>Contact Us</li>
+                    <li>Speed Test</li>
+                    <li>Legal Notices</li>
+                    <li>Netflix Originals</li>
+                </ul>
+                <p>Netflix Nigeria</p>
+            </div>
         </footer>
     )
 }
